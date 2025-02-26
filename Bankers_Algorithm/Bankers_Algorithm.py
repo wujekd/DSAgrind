8268e5cdb5c26acdd9d9da86b12a1cb175ba577b
Dominik Wujek <dom.wujek@gmail.com>
2025-02-26 20:47:56 +0000

    debug and add new process

--- Bankers_Algorithm/Bankers_Algorithm.py
+++ Bankers_Algorithm/Bankers_Algorithm.py
@@ -1,12 +1,9 @@
 
-
 #Bankers Algorithm
-
-
 
 def findSafeSequence(work, max, alloc, need):
     
-    process_count = len(work)
+    process_count = len(max)
     resource_count = len(max[0])
     
     p_status = [0] * process_count # is the process completed
@@ -44,17 +41,57 @@ def findSafeSequence(work, max, alloc, need):
             return False
                 
     print("System is in a safe state.")
-    print(safe_seq)
+    print(f"the safe sequence is: {safe_seq}")
     
     return True
 
 
 if __name__ =="__main__":
     
+    # init for the given n and m
+    columns = 3
+    rows = 3
+    
+    # get the inputs from a file ****
     work = [3,3,2]
     max = [[6,4,3],[3,2,2],[4,3,3]]
     alloc = [[2,2,1],[1,0,0],[2,1,1]]
-    need = [[4,2,2],[2,2,2],[2,2,2]]
+    
+    # add input validation ****
+    
+    # calculate need ***
+    need = [[max[i][j] - alloc[i][j] for j in range(len(max[0]))] for i in range(len(max))]
+
+    # find a safe sequence for the initial system snapshot
+    initCheck = findSafeSequence(work.copy(), max, alloc, need)
+    
+    option = "1" if initCheck else "0"
+    
+    while (option != "0"):
+        #findSafeSequence(work, max, alloc, need)
+        
+        
+        print("1 - add a process")
+        print("0 - exit")
+        option = input("insert choice: ")
+        
+        if (option == "1"):
+            # create a new process
+            np_max = list(map(int, input(f"enter {columns} integers to set the processes claimed max: ").split()))
+            np_alloc = list(map(int, input(f"enter {columns} integers to set resources allocated to the new process: ").split()))
+            
+            max.append(np_max)
+            alloc.append(np_alloc)
+            need.append([np_max[i] - np_alloc[i] for i in range(len(np_max))])
+            print("Added a new process to the system snapshot.")
+            rows = len(max)
+            check = findSafeSequence(work.copy(), max, alloc, need)
+            
+            
+                
+        elif (option != "0"):
+            print("invalid input!")
     
     
-    findSafeSequence(work, max, alloc, need)
+    
+    print("good bye!")
